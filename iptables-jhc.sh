51e72b0ce9b1b492d804f3edf119d3cd2c26b7de
uchuhimo <uchuhimo@outlook.com>
2021-03-23 18:23:59 +0800

    update iptables

--- iptables-jhc.sh
+++ iptables-jhc.sh
@@ -10,6 +10,7 @@ iptables -t nat -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t nat -A clash -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A clash -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A clash -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A clash -d 202.120.40.102 -j RETURN
 iptables -t nat -A clash -p tcp -j REDIRECT --to-port 7892
 iptables -t nat -A clash -p icmp -j REDIRECT --to-port 7892
 iptables -t nat -A PREROUTING -s 10.2.0.0/16 -p tcp -j clash
@@ -27,6 +28,7 @@ iptables -t nat -A clash_local -d 172.16.0.0/12 -j RETURN
 iptables -t nat -A clash_local -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A clash_local -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A clash_local -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A clash_local -d 202.120.40.102 -j RETURN
 iptables -t nat -A clash_local -p tcp -j REDIRECT --to-port 7892
 iptables -t nat -A clash_local -p icmp -j REDIRECT --to-port 7892
 iptables -t nat -A OUTPUT -p tcp -j clash_local
@@ -44,6 +46,7 @@ iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
 iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
+iptables -t mangle -A clash -d 202.120.40.102 -j RETURN
 iptables -t mangle -A clash -p udp -j TPROXY --on-port 7892 --tproxy-mark 1
 iptables -t mangle -A PREROUTING -s 10.2.0.0/16 -p udp -j clash
 iptables -t mangle -A PREROUTING -s 10.3.0.0/16 -p udp -j clash
